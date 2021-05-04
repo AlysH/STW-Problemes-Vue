43aa3db521c04e6337375a4b3dd6d89bde173cd0
Paula Sarqui <pfsarqui@gmail.com>
2021-05-04 18:18:04 +0200

    Implements 'Exercici_11'

---
+++ Exercici_11/app.js
@@ -0,0 +1,26 @@
+const options = {
+    template: `<words-to-list words="Lorem ipsum dolor sit amet"></words-to-list>`
+};
+
+const wordsToList = {
+    data: function () {
+        return {
+            separatedWords: this.words.split(' ')
+        }
+    },
+    props: {
+        words: String
+    },
+    template: `<div>
+        <ul>
+            <li v-for="word in separatedWords">{{ word }}</li>
+        </ul>
+    </div>`
+};
+
+
+const app = Vue.createApp(options);
+app.component('words-to-list', wordsToList);
+const vm = app.mount("#app");
+
+
