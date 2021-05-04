c5d0866bfb20cf0cfecad24f883cbd6f8dbca378
Paula Sarqui <pfsarqui@gmail.com>
2021-05-04 12:04:13 +0200

    Implements 'Exercici_4'

---
+++ Exercici_4/app.js
@@ -0,0 +1,17 @@
+const options = {
+    data: function () {
+        return {
+            characters: ""
+        };
+    },
+    methods: {
+        countCharacters() {
+            if (this.characters.length > 4) {
+                this.characters = "";
+            }
+        }
+    },
+    template: `<div><input v-model="characters" v-on:input="countCharacters"></div>`
+};
+
+const vm = Vue.createApp(options).mount('#app');
