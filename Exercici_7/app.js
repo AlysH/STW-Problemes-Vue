6ad872ba6101e3c8b2255c723db2682bd6665e05
Paula Sarqui <pfsarqui@gmail.com>
2021-05-04 13:12:32 +0200

    Implements 'Exercici_7'

---
+++ Exercici_7/app.js
@@ -0,0 +1,25 @@
+const options = {
+    data: function () {
+        return {
+            inputVal: 50,
+            a: false,
+            b: false,
+            c: false,
+            d: false
+        };
+    },
+    watch: {
+        a: function () {
+            this.b = this.a;
+        },
+        b: function () {
+            this.c = this.b;
+        },
+        c: function () {
+            this.d = this.c;
+        }
+    },
+    template: `<div><input type="checkbox" id="check" v-model="a"><label for="check">{{ a }} {{ b }} {{ c }} {{ d }}</label></div>`
+};
+
+const vm = Vue.createApp(options).mount('#app');
